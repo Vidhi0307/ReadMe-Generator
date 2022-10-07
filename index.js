58c31de1d17695e46aeb41c8651822492cc3d8c3
Akhilesh Sharma <vidhi.sydney07@gmail.com>
2022-10-08 02:03:56 +1100

    functionality implemented

--- index.js
+++ index.js
@@ -1,65 +1,118 @@
 // TODO: Include packages needed for this application
-const inquire = require('inquirer');
 const fs = require('fs');
-
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
+        name: "githubName",
+        message: "What is your GitHub username?.",
+        validate: userName => {
+            if (userName) {
+                return true;
+            } else {
+                console.log("Please provide your github Name.");
+                return false;
+            }
+        }
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is your email address?.",
+        validate: emailAddress => {
+            if (emailAddress) {
+                return true;
+            } else {
+                console.log("Please provide a email Address.");
+                return false;
+            }
+        }
+    }, {
+        type: "input",
         name: "title",
-        message: "What is the title of your project?"
+        message: "What is your project's name?.",
+        validate: titleName => {
+            if (titleName) {
+                return true;
+            } else {
+                console.log("Please provide a project name.");
+                return false;
+            }
+        }
     },
     {
         type: "input",
-        name: "projectDescription",
-        message: "Briefly give an explanation of your project. (Required)",
-
+        name: "projectDesc",
+        message: "Please write a short description of your project",
+        validate: description => {
+            if (description) {
+                return true;
+            } else {
+                console.log("Please write a short description.");
+                return false;
+            }
+        }
     },
     {
         type: "input",
-        name: "installInfo",
-        message: "Provide any installation instructions here:",
+        name: "installInstructions",
+        message: "What command should be run to install dependencies?",
     },
     {
         type: 'checkbox',
         name: 'license',
         message: 'Which license is used for this project:',
         choices: ['Apache', 'Mozilla', 'MIT', 'GNU', 'Boost', 'ISC'],
-
+        validate: choicesLength => {
+            if (choicesLength.length <= 1) {
+                return true;
+            } else {
+                return "Please select one license!";
+            }
+        }
     },
     {
         type: "input",
-        name: "usageInfo",
-        message: "Describe the main use case for this project/application:",
+        name: "userContribution",
+        message: "What does the user need to know about contributing to the repo?",
     },
     {
         type: "input",
-        name: "contributionsInfo",
-        message: "Are their any guidlines for contributing to this project:",
+        name: "repoInfo",
+        message: " What does the user need to know about using the repo?",
     },
     {
         type: "input",
-        name: "testsInfo",
-        message: "Which tests would you like to include:",
-    },
-    {
-        type: "input",
-        name: "githubUsername",
-        message: "What is your GitHub username (Required)?",
-    },
-    {
-        type: "input",
-        name: "emailInfo",
-        message: "What is a good email someone can reach you(Required)?",
+        name: "tests",
+        message: "What command should be run to run tests?",
     }
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile("./readme/README.md", generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Success!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+        .then(function (answer) {
+            const fileName =
+                answer.title
+                    .split(' ')
+                    .join('') + '.md';
+
+            writeToFile(fileName, answer);
+        });
+}
 
 // Function call to initialize app
 init();
